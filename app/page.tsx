afe1f8d77c852e975048873ede09911408e1d51b
RicardoCoutoMadureira34 <ricardomadureira34@gmail.com>
2025-07-17 23:58:12 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
-import SideNavigationBar from "./components/sideNavigationBar";
 
 export default function Home() {
   return (
-    <SideNavigationBar>
       <div className="pt-10 px-80 py-10">
         <h1>Gestao de Seguros</h1>
         <p>HOME PAGE</p>
       </div>
-    </SideNavigationBar>
   );
 }
