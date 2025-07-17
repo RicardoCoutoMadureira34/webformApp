e47ce3a5ac5d5d9a2dbf7a6c09f7d06ef4c1833a
RicardoCoutoMadureira34 <ricardomadureira34@gmail.com>
2025-07-18 00:16:46 +0100

    changes in structure of project

--- app/ExpenseManagement/home/page.tsx
+++ app/ExpenseManagement/home/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
       <div className="pt-10 px-80 py-10">
-        <h1>Gestao de Seguros</h1>
+        <h1>Gestao de despesas</h1>
         <p>HOME PAGE</p>
       </div>
   );
